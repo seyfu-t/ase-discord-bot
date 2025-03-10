1f9a847d449a46b913926f79d0e3c7f97d03c738
Maximilian <maximilian.toebben@gmail.com>
2025-03-11 00:31:39 +0100

    fix: flake8 autopep8 compliance

--- src/main.py
+++ src/main.py
@@ -1,14 +1,15 @@
+from config import check_env_vars
 import logging
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-from config import check_env_vars
 
 def main():
     check_env_vars()
 
     logger.info("Environment validated successfully")
+
 
 if __name__ == "__main__":
     main()
