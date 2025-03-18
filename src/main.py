5416ab6d47750fde58190ddf1b7480c0f6876e6e
seyfu-t <git@seyfu-t.me>
2025-03-18 14:47:32 +0100

    ASE2-69 feat: add/unify logging

--- src/main.py
+++ src/main.py
@@ -1,16 +1,16 @@
 from bot.discord_bot import run_bot
 from config import Config, check_env_vars
+from logger_config import setup_logger
 import logging
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+
+setup_logger()
+logger = logging.getLogger("Main")
 
 
 def main():
     check_env_vars()
     cfg = Config()
-
-    logger.info("Environment validated successfully")
 
     run_bot(cfg.DISCORD_TOKEN)
 
