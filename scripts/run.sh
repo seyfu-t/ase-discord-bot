f9fec674ca2fb7366c9d447f805cdb3b79e25d98
max <maximilian.toebben@gmail.com>
2025-03-21 15:09:51 +0100

    ASE2-85 feat(script): add run script

---
+++ scripts/run.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Exit on fail
+set -e
+
+# Required so that the script can be started from anywhere
+SCRIPT_DIR="$(cd "$(`dirname` "${BASH_SOURCE[0]}")" && cd .. && pwd)"
+
+# Activate venv
+source "$SCRIPT_DIR/.venv/bin/activate"
+
+# Run
+python "$SCRIPT_DIR/src/main.py"
+
+# Deactivate venv
+deactivate
