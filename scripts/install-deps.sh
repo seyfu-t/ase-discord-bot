630b772341ba466eadf94934bedbda5a0162257b
max <maximilian.toebben@gmail.com>
2025-03-21 15:09:23 +0100

    ASE2-86 feat(script): add dependecy installer (inside venv)

---
+++ scripts/install-deps.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# Exit on fail
+set -e
+
+# Required so that the script can be started from anywhere
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && cd .. && pwd)"
+
+# venv path inside project folder
+VENV_DIR="$SCRIPT_DIR/.venv"
+
+# Find Python
+if command -v python3 &>/dev/null; then
+    PYTHON_EXEC="python3"
+elif command -v python &>/dev/null; then
+    PYTHON_EXEC="python"
+else
+    echo "Error: Python is not installed."
+    exit 1
+fi
+
+# Check if venv already exists
+if [ ! -d "$VENV_DIR" ]; then
+    echo "Creating virtual environment in $VENV_DIR"
+    "$PYTHON_EXEC" -m venv "$VENV_DIR"
+fi
+
+# Activate venv
+source "$VENV_DIR/bin/activate"
+
+# Install deps via poetry
+cd "$SCRIPT_DIR"
+poetry install --no-root
+
+# Deactivate the venv
+deactivate
