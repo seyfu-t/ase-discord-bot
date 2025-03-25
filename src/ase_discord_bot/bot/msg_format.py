649db08be7d13153f4f8506862495fd71466a9e3
domme7 <dominik.hu@outlook.de>
2025-03-25 16:55:46 +0100

    ASE2-110 feat: add helper functions to format movie message

---
+++ src/ase_discord_bot/bot/msg_format.py
@@ -0,0 +1,36 @@
+from datetime import date
+from ase_discord_bot.ai.summary import summarize
+from ase_discord_bot.api_util.model.responses import Movie, MovieResponse, TVShowResponse
+from ase_discord_bot.config import Config
+
+
+def format_recommendation(cfg: Config, response: MovieResponse | TVShowResponse):
+    if type(response) is MovieResponse:
+        formatted_response: str = ""
+
+        # for movie in response.results:
+        movie = response.results[0]
+        formatted_response += _format_recommendation_movie(cfg, movie)
+        formatted_response += "\n"
+
+        return formatted_response
+
+
+def _format_recommendation_movie(cfg: Config, movie: Movie) -> str:
+    formatted_response: list[str] = []
+
+    formatted_response.append(f"### *{movie.title}*")
+
+    if movie.title != movie.original_title:
+        formatted_response.append(f"-# _{movie.original_title}_")
+
+    formatted_response.append(f"- Released: {date.fromisoformat(movie.release_date).strftime('%d.%sm.%Y')}")
+
+    ai_summary = summarize(cfg, movie)
+    formatted_response.append(f"- Description: {ai_summary}")
+
+    # if movie.poster_path:
+    #     poster_url = api_calls.get_poster_url(cfg, movie.poster_path[1:])
+    #     formatted_response.append(f"![{movie.title}.jpg]({poster_url})")
+
+    return "\n".join(formatted_response)
