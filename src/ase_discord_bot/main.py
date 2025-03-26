eef1c0d6ad324ebeffdef222fd4834904581ca45
seyfu-t <git@seyfu-t.me>
2025-03-26 13:40:20 +0100

    ASE2-123 refactor: init central config registry in main

--- src/ase_discord_bot/main.py
+++ src/ase_discord_bot/main.py
@@ -2,6 +2,8 @@ from ase_discord_bot.bot.discord_bot import run_bot
 from ase_discord_bot.config import Config, check_env_vars, setup_logger
 import logging
 
+from ase_discord_bot.config_registry import set_config
+
 
 setup_logger()
 logger = logging.getLogger("Main")
@@ -10,6 +12,7 @@ logger = logging.getLogger("Main")
 def main():
     check_env_vars()
     cfg = Config()
+    set_config(cfg)
 
     run_bot(cfg)
 
