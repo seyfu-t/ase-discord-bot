43785a915dc3493e734729c3a474ca7999b8c50e
seyfu-t <git@seyfu-t.me>
2025-03-26 14:35:41 +0100

    ASE2-127 refactor: migrate to using movie filter dataclass

--- src/ase_discord_bot/api_util/api_calls.py
+++ src/ase_discord_bot/api_util/api_calls.py
@@ -1,7 +1,6 @@
 import requests
 
-from typing import Optional
-from ase_discord_bot.api_util.model.languages import Language
+from ase_discord_bot.api_util.model.filters import MovieFilter
 from ase_discord_bot.api_util.model.responses import MovieResponse
 from ase_discord_bot.config import Config
 from datetime import date
@@ -13,14 +12,8 @@ def get_poster_url(cfg: Config, path: str) -> str:
     return (cfg.TMDB_IMAGES_BASE_URL / path).human_repr()
 
 
-def get_recommended_movie(
-    genre: int,
-    year: Optional[int],
-    min_year: Optional[int],
-    max_year: Optional[int],
-    original_language: Optional[Language],
-) -> MovieResponse | int:
-    response = _request_movie_recommendation(genre, year, min_year, max_year, original_language)
+def get_recommended_movie(movie_filter: MovieFilter) -> MovieResponse | int:
+    response = _request_movie_recommendation(movie_filter)
 
     if response.status_code != 200:
         return response.status_code
@@ -30,29 +23,23 @@ def get_recommended_movie(
     return movie_data
 
 
-def _request_movie_recommendation(
-    genre: int,
-    year: Optional[int],
-    min_year: Optional[int],
-    max_year: Optional[int],
-    original_language: Optional[Language],
-) -> requests.Response:
+def _request_movie_recommendation(movie_filter: MovieFilter) -> requests.Response:
     cfg = get_config()
-    query_dict: dict[str, str | int] = {"with_genres": genre}
+    query_dict: dict[str, str | int] = {"with_genres": movie_filter.genre}
 
-    if year:
-        query_dict["primary_release_year"] = year
+    if movie_filter.year:
+        query_dict["primary_release_year"] = movie_filter.year
 
-    if min_year:
-        min_year_date = date(min_year, 1, 1)
+    if movie_filter.min_year:
+        min_year_date = date(movie_filter.min_year, 1, 1)
         query_dict["primary_release_date.gte"] = min_year_date.strftime("%Y-%m-%d")
 
-    if max_year:
-        max_year_date = date(max_year, 12, 31)
+    if movie_filter.max_year:
+        max_year_date = date(movie_filter.max_year, 12, 31)
         query_dict["primary_release_date.lte"] = max_year_date.strftime("%Y-%m-%d")
 
-    if original_language:
-        query_dict["with_original_language"] = original_language.iso_code
+    if movie_filter.original_language:
+        query_dict["with_original_language"] = movie_filter.original_language.iso_code
 
     query_url = (cfg.TMDB_API_BASE_URL / "discover/movie").human_repr()
 
