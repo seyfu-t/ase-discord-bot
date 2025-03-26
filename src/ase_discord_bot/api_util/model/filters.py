f57d0dbc74852681c5560663c541c156527c4b63
seyfu-t <git@seyfu-t.me>
2025-03-26 14:33:47 +0100

    ASE2-126 refactor: added filter dataclass for movies

---
+++ src/ase_discord_bot/api_util/model/filters.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass
+from typing import Optional
+
+from ase_discord_bot.api_util.model.languages import Language
+
+
+@dataclass
+class MovieFilter:
+    genre: int
+    year: Optional[int] = None
+    min_year: Optional[int] = None
+    max_year: Optional[int] = None
+    original_language: Optional[Language] = None
