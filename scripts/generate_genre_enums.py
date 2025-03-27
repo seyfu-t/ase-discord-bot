448abcd9dc037cbcf80efc110591b7468d445154
seyfu-t <git@seyfu-t.me>
2025-03-27 11:59:44 +0100

    ASE2-136 rename: use 'tvshow' only instead of mixing 'series' and 'tvshow'

--- scripts/generate_genre_enums.py
+++ scripts/generate_genre_enums.py
@@ -10,14 +10,14 @@ def format_enum_name(name):
     return key
 
 
-def generate_genres_enums(movie_json, series_json):
+def generate_genres_enums(movie_json, tvshow_json):
     file_lines = []
     movie_lines = []
-    series_lines = []
+    tvshow_lines = []
 
     # Sort by id ascending
     movie_data = sorted(movie_json["genres"], key=lambda x: x["id"])
-    series_data = sorted(series_json["genres"], key=lambda x: x["id"])
+    tvshow_data = sorted(tvshow_json["genres"], key=lambda x: x["id"])
 
     # Convert each entry into a single line of an enum
     for genre in movie_data:
@@ -27,12 +27,12 @@ def generate_genres_enums(movie_json, series_json):
 
         movie_lines.append(f"    {enum_name} = (\"{name}\", {genre_id})")
 
-    for genre in series_data:
+    for genre in tvshow_data:
         genre_id = genre["id"]
         name = genre["name"]
         enum_name = format_enum_name(name)
 
-        series_lines.append(f"    {enum_name} = (\"{name}\", {genre_id})")
+        tvshow_lines.append(f"    {enum_name} = (\"{name}\", {genre_id})")
 
     # Assemble file
     file_lines = "from ase_discord_bot.api_util.model.genre_base import GenreEnum\n"
@@ -42,8 +42,8 @@ def generate_genres_enums(movie_json, series_json):
     file_lines += "\n".join(movie_lines)
     file_lines += "\n"
     file_lines += "\n"
-    file_lines += "class SeriesGenre(GenreEnum):\n"
-    file_lines += "\n".join(series_lines)
+    file_lines += "class TvShowGenre(GenreEnum):\n"
+    file_lines += "\n".join(tvshow_lines)
 
     return file_lines
 
@@ -53,10 +53,10 @@ if __name__ == "__main__":
 
     with open(PROJECT_ROOT_PATH / "assets/movie_genres.json", "r", encoding="utf-8") as movie_file:
         movie_json = json.load(movie_file)
-    with open(PROJECT_ROOT_PATH / "assets/series_genres.json", "r", encoding="utf-8") as series_file:
-        series_json = json.load(series_file)
+    with open(PROJECT_ROOT_PATH / "assets/tvshow_genres.json", "r", encoding="utf-8") as tvshow_file:
+        tvshow_json = json.load(tvshow_file)
 
-    enum_str = generate_genres_enums(movie_json, series_json)
+    enum_str = generate_genres_enums(movie_json, tvshow_json)
 
     with open(PROJECT_ROOT_PATH / "src/ase_discord_bot/api_util/model/genres.py", "w", encoding="utf-8") as f:
         f.write(enum_str)
