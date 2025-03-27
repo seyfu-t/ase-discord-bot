9d0fbd043aba03acd9dcb8b7ae1283e683abe829
seyfu-t <git@seyfu-t.me>
2025-03-27 09:48:55 +0100

    ASE2-133 rename: check_env_vars -> check_and_load_env_vars

--- src/ase_discord_bot/main.py
+++ src/ase_discord_bot/main.py
@@ -1,5 +1,5 @@
 from ase_discord_bot.bot.discord_bot import run_bot
-from ase_discord_bot.config import Config, check_env_vars, setup_logger
+from ase_discord_bot.config import Config, check_and_load_env_vars, setup_logger
 import logging
 
 from ase_discord_bot.config_registry import set_config
@@ -10,7 +10,7 @@ logger = logging.getLogger("Main")
 
 
 def main():
-    check_env_vars()
+    check_and_load_env_vars()
     cfg = Config()
     set_config(cfg)
 
